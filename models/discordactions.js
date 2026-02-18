30171c3ccffdf75db33aa938baf08afb1d2e056c
vinit717 <vinit224488@gmail.com>
2026-02-18 21:42:04 +0530

    fix: change allMavens declaration from let to const for better immutability

--- models/discordactions.js
+++ models/discordactions.js
@@ -404,7 +404,7 @@ const updateIdleUsersOnDiscord = async (dev) => {
   let allUsersHavingGroupIdle = [];
   let groupIdleRole;
   let groupIdleRoleId;
-  let allMavens = [];
+  const allMavens = [];
 
   try {
     groupIdleRole = await getGroupRole("group-idle");
